07d786b324449fcd474ec980f3b354bf0ab54dae
gureisu81 <20f02581@gmail.com>
2025-02-28 07:55:54 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,15 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+	
+	public static void main(String[] args) {
+		
+		Item car = new Item();
+		car.gearChange(3);
+		car.run();
+		
+
+	}
+
+
+}
